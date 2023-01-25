b2cce7d574d67a4b7fc3be61eaf0e34f2912fcae
Santiago Sanchez <santiagosancheze8.1@gmail.com>
2023-01-25 10:45:37 -0500

    terminar practica

--- practices/resource/js/app.js
+++ practices/resource/js/app.js
@@ -1,8 +1,20 @@
-angular.module("FinalApp", ["lumx", "ngRoute"])
+angular.module("FinalApp", ["lumx", "ngRoute", "ngResource"])
     .config(function ($routeProvider) {
         $routeProvider
             .when("/", {
                 controller: "MainController",
-                templateUrl: "templates/home.html"
+                templateUrl: "resource/templates/home.html"
+            })
+            .when("/post/:id", {
+                controller: "PostController",
+                templateUrl: "resource/templates/post.html"
+            })
+            .when("/posts/new", {
+                controller: "NewPostController",
+                templateUrl: "resource/templates/post_form.html"
+            })
+            .when("/posts/edit/:id", {
+                controller: "EditPostController",
+                templateUrl: "resource/templates/post_form.html"
             })
     })
--- practices/resource/js/controllers.js
+++ practices/resource/js/controllers.js
@@ -1,4 +1,41 @@
 angular.module("FinalApp")
-    .controller("MainController", function ($scope) {
+    .controller("MainController", function ($scope, $resource, PostResource) {
+        User = $resource("https://jsonplaceholder.typicode.com/users/:id", { id: "@id" })
 
+        $scope.posts = PostResource.query();
+        $scope.users = User.query();
+        $scope.removePost = function (post) {
+            PostResource.delete({ id: post.id }, function (data) {
+                console.log(data)
+                $scope.posts = $scope.posts.filter((element) => {
+                    return element.id !== post.id
+                })
+            })
+        }
+    })
+    .controller("PostController", function ($scope, $routeParams, PostResource) {
+        $scope.post = PostResource.get({ id: $routeParams.id });
+
+    })
+    .controller("NewPostController", function ($scope, PostResource, $location) {
+        $scope.post = {}
+        $scope.title = "Create post!"
+        $scope.savePost = function () {
+            // console.log(PostResource.save({ data: $scope.post }))
+            PostResource.save({ data: $scope.post }, function (data) {
+                console.log(data)
+                $location.path("/")
+            })
+        }
+    })
+    .controller("EditPostController", function ($scope, PostResource, $routeParams, $location) {
+        $scope.post = PostResource.get({id: $routeParams.id})
+        $scope.title = "Edit post!"
+        $scope.savePost = function () {
+            // console.log(PostResource.save({ data: $scope.post }))
+            PostResource.update({id: $routeParams.id}, { data: $scope.post }, function (data) {
+                console.log(data)
+                $location.path("/post/" + $routeParams.id)
+            })
+        }
     })
